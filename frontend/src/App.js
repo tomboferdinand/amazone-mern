3f3a7b3269522e0e7cb56fbcf36530328b7ac80b
Your Name <tomboferdinand7@gmail.com>
2023-10-20 14:30:06 +0200

    test0

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Lazan'ny Betsileo</a>
+      </header>
+      <main>list</main>
+    </div>
+  );
+}
+
+export default App;
